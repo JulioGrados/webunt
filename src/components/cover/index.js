f79c7556e612deda15a6cec8e262c7116308c37a
Julio Giampiere Grados Caballero <juliogiampieregradoscaballero@Mac-mini-de-Julio.local>
2025-06-18 22:09:41 -0500

    cambios foto

--- src/components/cover/index.js
+++ src/components/cover/index.js
@@ -1,27 +1,44 @@
 import { Container } from "../../styles/main"
 import Header from "../header"
-import { useRef, useEffect } from 'react'
-import { BackgroundVideo, Content, CoverButtonOne, CoverButtons, CoverButtonTwo, CoverContent, CoverStyled, CoverText, CoverTitle, Main, Overlay } from "./styles"
+import { useRef, useEffect, useState } from 'react'
+import { BackgroundVideo, Content, CoverButtonOne, CoverButtons, CoverButtonTwo, CoverContent, CoverStyled, CoverText, CoverTitle, Main, Overlay, VideoPlaceholder } from "./styles"
 
 const Cover = () => {
+    const [videoLoaded, setVideoLoaded] = useState(false)
     const videoRef = useRef(null);
 
     useEffect(() => {
-    if (videoRef.current) {
-        videoRef.current.muted = true;
-        videoRef.current.play().catch(error => {
-        console.error('Error al reproducir el video:', error);
-        });
-    }
+        if (videoRef.current) {
+            videoRef.current.muted = true;
+            videoRef.current.play().catch(error => {
+            console.error('Error al reproducir el video:', error);
+            });
+        }
     }, []);
+    const handleVideoLoaded = () => {
+        setVideoLoaded(true)
+        if (videoRef.current) {
+          const playPromise = videoRef.current.play()
+          if (playPromise !== undefined) {
+            playPromise.catch(error => {
+              console.warn("Reproducción automática bloqueada:", error)
+            })
+          }
+        }
+    }
     return (
         <CoverStyled>
+            {!videoLoaded && (
+                <VideoPlaceholder src="/static/img/home.png" alt="Placeholder" />
+            )}
             <BackgroundVideo 
                 ref={videoRef}
-                autoplay 
-                muted 
-                loop 
-                playsinline
+                autoPlay
+                muted
+                loop
+                playsInline
+                onLoadedData={handleVideoLoaded} // se ejecuta cuando el video está listo
+                style={{ opacity: videoLoaded ? 1 : 0, transition: 'opacity 0.5s ease-in-out' }}
             >
                 <source src={`/static/img/home.mp4`} type="video/mp4" />
             </BackgroundVideo>
--- src/components/cover/styles.js
+++ src/components/cover/styles.js
@@ -189,3 +189,13 @@ export const CoverCircle = styled.div`
     transform: translateX(-50%);
     animation: ${blink} 1s infinite;
 `
+
+export const VideoPlaceholder = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 0;
+`
